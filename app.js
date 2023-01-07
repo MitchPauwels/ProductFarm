4ef3b5f5c65e89d6ad32eeb459fa113f855d1252
Mitch Pauwels <Mitch.Pauwels@hotmail.com>
2023-01-07 14:55:20 +0000

    adding more routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
 
 // Set the view engine to EJS
 app.set("view engine", "ejs");
@@ -9,12 +10,36 @@ app.set("views", path.join(__dirname, "views"));
 
 // A middleware function that logs the request method and URL
 app.use((req, res, next) => {
-  console.log(req.method, req.url);
+  console.log(req.method, req.url, res.statusCode);
+  next();
 });
+// Parse request bodies as JSON
+app.use(bodyParser.json());
+
+// Parse request bodies as URL-encoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // A route that renders the index view
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+// A route that renders the subreddit view
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.send(`Subreddit of ${subreddit}`);
+});
+
+// A route that renders the subreddit comments view
+app.get("/r/:subreddit/comments/:postID", (req, res) => {
+  const { subreddit, postID } = req.params;
+  res.send(`${subreddit} comment section ID:${postID}`);
+});
+
+// A route that renders the index view
+app.get("/project/:projectname", (req, res) => {
+  const { projectname } = req.params;
+  res.render("project", { projectname });
 });
 
 app.listen(3000, () => {
